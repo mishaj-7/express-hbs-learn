c8c91b3828ba22d7acc830aec09b2e71451d5e2f
Mishaj__k <amstig100@gmail.com>
2024-01-29 14:57:44 +0530

    sample

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'mishaj"s express js',
+    passsion: 'adrenet mern explorer'
+  });
 });
 
 module.exports = router;
